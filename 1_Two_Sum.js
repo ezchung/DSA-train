245f33fe96a2c96cf8cd22b6fce0232a7ceeab5e
Ezra Chung <ezra.chung97@gmail.com>
2023-02-07 17:28:27 -0800

    1_Two_Sum

---
+++ 1_Two_Sum.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let length = nums.length
+    for(let index in nums){
+        let current = +index + 1
+        while(current < length){
+            if(nums[index] + nums[current] === target){
+                return [+index, current]
+            }
+            current++;
+        }
+    }
+};
+
+/* 
+ Problem: With the array of ints and a target number, find which two numbers add up to the target number
+
+ Test Case
+    [1,2,3,4,5], 4 => [0,2]
+    [1,2], 3 => [0,1]
+    [1,2,4,3], 3 => [0,1]
+
+ Notes
+    Each input would have exactly one solution. May not use the same element twice
+
+ Write it out
+    Take a look at the first number
+        Take a look at the second number
+            if the two add up to target number, return index
+*/
