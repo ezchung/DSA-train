e437f7da185e60412469e76817e19de5113d037d
Ezra Chung <ezra.chung97@gmail.com>
2023-02-07 21:51:19 -0800

    217_Contains_duplicates

---
+++ 217_Contains_Duplicates.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var containsDuplicate = function(nums) {
+    
+};
+
+/**
+ Problem
+ - If the value contains duplicates, return true
+
+ Test Case
+ - [1,2,3,1] => true
+ - [1,2,3,4] => false
+
+ PsuedoCode
+ - Use a set
+ - if length of set is not equal to length of original nums, true, if equal (means all distinct), return false
+ */
