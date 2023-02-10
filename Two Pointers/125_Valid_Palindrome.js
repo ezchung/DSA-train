ba84a9b87cd3f2fadc65600f7f3dbb902d2f015b
Ezra Chung <ezra.chung97@gmail.com>
2023-02-10 12:45:32 -0800

    Create new folder for Two Pointers

---
+++ Two Pointers/125_Valid_Palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    
+};
+
+/**
+Problem
+- return true if palindrome, if not false
+
+Code
+- iterate through string and take out anything that is not an alphabet
+- lower case the string
+- create two pointers
+- while start is not equal to end
+    - check if the two are equal
+    - if not return false
+    add 1 to start, minus 1 to end
+- return true
+*/
