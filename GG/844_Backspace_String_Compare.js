98e34fc25fc95126e57d248e744627d0038c2b1d
Ezra Chung <ezra.chung97@gmail.com>
2023-03-06 19:58:33 -0800

    Notes for code

--- GG/844_Backspace_String_Compare.js
+++ GG/844_Backspace_String_Compare.js
@@ -6,3 +6,22 @@
 var backspaceCompare = function(s, t) {
     
 };
+
+/**
+Question: 
+    Given two strings, return true if two are equal. # means backspace so delete the prior element if it exists
+
+PsuedoCode
+Create a function that sends one each and returns with the backspaces functionality invoked
+    create a new array
+    iterate through string
+        if element has a hashtag
+            remove the element from the back
+            so array.pop()
+        else
+            add to array
+    return array.join("");
+
+check if two are equal to each other
+
+ */
