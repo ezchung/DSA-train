c5854efb670180c269e28d2d62a611cc8967f328
Ezra Chung <ezra.chung97@gmail.com>
2023-03-07 12:50:41 -0800

    Notes

--- GG/2325_Decode_the_Msg.js
+++ GG/2325_Decode_the_Msg.js
@@ -6,3 +6,20 @@
 var decodeMessage = function(key, message) {
     
 };
+
+/**
+Question
+    Get the key and the message (already encoded)
+    Decode the message and return the message translated
+
+PsuedoCode
+    take out anything that is not a character from the key
+    make it a set so no duplicates
+    create an alphabet string
+    create an object
+    create a variable to keep track of alphabet pointer
+    create return message string
+    iterate through message
+        identify the location of the char
+        with that index, go to alphabet[index] and add that char to msg
+*/
