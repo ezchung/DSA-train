352cf17ae5f5e34b3f6b5539d99362496dc4b613
Ezra Chung <ezra.chung97@gmail.com>
2023-03-17 11:47:53 -0700

    New Code

--- Two Pointers/15_3Sum.js
+++ Two Pointers/15_3Sum.js
@@ -24,7 +24,7 @@ var threeSum = function(nums) {
  iterate through the array
  look at the first position and the second position,
     add them together and find what number we need to get 0
-    find the number
+    find the number and check that it is not the same element as first or second
     add to array sorted
     check to see if array exists in memo
         if it does, skip
@@ -33,4 +33,27 @@ var threeSum = function(nums) {
     once the second pointer reaches the end
         first pointer moves up and the new array is sliced from start+1 to finish
         continue until array length is equal to 3
+
+New Code
+*Note: Sorting so that we know what the order is where to look
+- sort the array nums
+- create a variable array
+- if nums array is < 3, return variable array
+- Iterate through the nums array
+    - left is i+1
+    - right is from the back
+    - if i is greater than 0 and nums[i] === nums[i-1] continue
+    - while left is greater right
+        create variable sum that takes i + left + right
+        if sum === 0
+            create an array of the three values
+            push the result
+            increment left
+            while num[left] === nums[left-1] && left < right, increment left
+                *Note: Keeps on adding left til the values are different
+        else if sum greater than 0
+            decrement right
+        else
+            increment left (meaning less than 0 so no result)
+- return result
 */
