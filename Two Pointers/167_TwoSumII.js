56679bc92034c6b33a2680ec41173b08d4a2a2d7
Ezra Chung <ezra.chung97@gmail.com>
2023-03-28 17:15:08 -0700

    Psuedo Code 167

--- Two Pointers/167_TwoSumII.js
+++ Two Pointers/167_TwoSumII.js
@@ -6,3 +6,19 @@
 var twoSum = function(numbers, target) {
     
 };
+
+/**
+PsuedoCode
+since they are already sorted, from the back, check to see if left and right add up to target
+while left is less than right
+    if array left + array right is equal to target
+        add to return array
+        left++
+        right = end of numbers array
+    else if array left + array right is greater than target
+        right--;
+    else
+        return numbers array
+return numbers array
+        
+*/
