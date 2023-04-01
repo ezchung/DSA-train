596f1b3435851720ee040f5550082f7065d0879f
Ezra Chung <ezra.chung97@gmail.com>
2023-03-31 19:45:44 -0700

    PsuedoCode but no sliding window

--- Sliding Window/121_Best_Time_Stock.js
+++ Sliding Window/121_Best_Time_Stock.js
@@ -5,3 +5,19 @@
 var maxProfit = function(prices) {
     
 };
+
+/**
+What are the things that we will need to know
+- the highest difference
+- look at the start number
+- look at the end number
+
+declare currentMaxOutput = 0;
+two pointers method
+iterate through array from i to length-2
+    iterate through from j as i+1 to length-1
+        check the difference. 
+        if greater than 0, check if greater than currentMaxOutput
+
+return currentMaxOutput
+*/
