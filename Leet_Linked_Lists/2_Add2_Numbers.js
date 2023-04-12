d895419781a91f5272453dde0d6a7d21526461c7
Ezra Chung <ezra.chung97@gmail.com>
2023-04-11 19:54:39 -0700

    Process with problem 2

--- Leet_Linked_Lists/2_Add2_Numbers.js
+++ Leet_Linked_Lists/2_Add2_Numbers.js
@@ -11,8 +11,26 @@
  * @return {ListNode}
  */
 var addTwoNumbers = function(l1, l2) {
-    
+    let num1 = +createArr(l1);
+    let num2 = +createArr(l2);
+    let result = num1 + num2;
+
 };
+
+let createArr = function(linkedList){
+    let value = linkedList;
+    let organizedArr = []
+    while(value !== null){
+        organizedArr.unshift(value.val);
+        value = value.next;
+    }
+    return organizedArr.join("");
+}
+
+let createLL = function(num){
+    let newNode = new Node();
+    
+}
 
 /* 
 iterate through the linked lists and create a string of the numbers
@@ -23,4 +41,7 @@ while variable is not null
     variable = variable.next;
 
 make string to number and add the two strings
+
+create an array for the iteration of LL
+create LL for return result
 */
