2e3eb45f14284f4800212bd81dec2da736abf87c
Ezra Chung <ezra.chung97@gmail.com>
2023-04-18 01:14:02 -0700

    Psuedo for min stack prob 155

--- Stacks/155_min_stack.js
+++ Stacks/155_min_stack.js
@@ -39,3 +39,27 @@ MinStack.prototype.getMin = function() {
  * var param_3 = obj.top()
  * var param_4 = obj.getMin()
  */
+
+/**
+ hint: think of each node having a corresponding min value
+ push
+ - in array for min, check the value and insert the value where it belongs
+ 
+ with min method
+ - should be at the top of this.min array so I can grab in constant time
+ - Question is how do I continually organize it with push and pop
+    Does the organization have to be in constant time as well? if we use a different method is that being hacky?
+
+Solution
+- in the array, attach the two together
+    When pushed, create an object with the value and the min (look at current length of stack, give option of the incoming value or the min between the two)
+    In min, we will look at the last entry and return that min
+
+    - Whenever you add a val, add to min as well. 
+        check if less than min, 
+            yes, add this to front of min, move current min to index 1
+            no, but less than second min, add to second min, remove current second min
+                NM, when a pop is used at that is the current min, we start missing one. If we pop consecutively, missing two. 
+    - Conclusion: this upper code will not scale well
+
+*/
