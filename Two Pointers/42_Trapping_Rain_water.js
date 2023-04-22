d1b6b9175e061409025e8e94956395d81c278f4e
Ezra Chung <ezra.chung97@gmail.com>
2023-04-22 15:03:16 -0700

    Initial notes

--- Two Pointers/42_Trapping_Rain_water.js
+++ Two Pointers/42_Trapping_Rain_water.js
@@ -5,3 +5,18 @@
 var trap = function(height) {
     
 };
+
+/**
+Notes:
+Given array representing height
+What is the maximum amount of water that this can trap
+The algorithm to figure out how many it can trap
+    Height on both sides must exist for water to be trapped
+    min(left and right) - current height
+    - determines how much water we can trap at position i
+        if negative, then cannot trap water in that location
+
+Idea: look at the height (height is like rocks). The space above is the borders around them allow it are what causes water to be trapped
+Look at everything in between
+
+**/
