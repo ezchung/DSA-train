ed3c7596230a6ab74f90734af25b07d584e0452a
Ezra Chung <ezra.chung97@gmail.com>
2023-05-19 17:01:27 -0700

    Notes for the question

--- Sliding Window/3_Longest_Substr_WO_Repeat_chars.js
+++ Sliding Window/3_Longest_Substr_WO_Repeat_chars.js
@@ -5,3 +5,27 @@
 var lengthOfLongestSubstring = function(s) {
     
 };
+
+/**
+Question
+    Find the longest substring witout repeat characters
+
+Notes
+    Would capital letters be considered same?
+    if the string is at least one, the minimum would be 1
+    Sliding window
+
+General Psuedo
+    two pointers
+        one for start of substring the next for the end of substring 
+    currentMax
+    iterate through string(while end < string.length)
+        check the end of substring and the one before
+        if they are not the same, currentMax is max(currentMax, end-start)
+        if they are the same, start is now equal to the end
+        end++;
+    return currentMax
+
+PsuedoCode
+
+*/
