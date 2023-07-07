37e26395db75cf5b021a496b1c57f9d219e12892
Ezra Chung <ezra.chung97@gmail.com>
2023-07-06 22:45:11 -0700

    Result

--- Sliding Window/76_Min_Window_SubStr.js
+++ Sliding Window/76_Min_Window_SubStr.js
@@ -4,7 +4,47 @@
  * @return {string}
  */
 var minWindow = function(s, t) {
+    if(t.length > s.length) return "";
+
+    let string = s.toLowerCase();
+    let substr = t.toLowerCase();
     
+    let charsNeeded = {};
+    for(let idx in t){
+        charsNeeded[t[idx]] = (charsNeeded[t[idx]] || 0) + 1
+    }
+
+    let leftP = 0,
+        rightP = 0,
+        min = [];
+    
+    let numOfNeeded = t.length;
+
+    while(rightP < s.length){
+        if(charsNeeded[s[rightP]] > 0){
+            numOfNeeded--;
+            charsNeeded[s[rightP]]--;
+        }
+        if(rightP-leftP >= t.length-1){
+            if(numOfNeeded === 0){
+                let dist = rightP-leftP;
+                let currentMin = (min[0] || Infinity)-(min[1] || Infinity);
+                let res = dist >= currentMin;
+                if(res) min = [rightP,leftP];
+                if(s[leftP] in charsNeeded){
+                    numOfNeeded++;
+                    charsNeeded[s[leftP]]++;
+                }
+                leftP++;
+            }
+        }
+        while(leftP < rightP || !(s[leftP] in charsNeeded)){
+            if(s[leftP] in charsNeeded) continue;
+            else leftP++;
+        }
+        rightP++;
+    }
+    return min
 };
 
 /**
@@ -67,3 +107,139 @@ while rightP < s.length
  - If leftP is a valid char,
         add the charsNeeded
   */
+
+/**************** First Iteration ********/
+/******************** Error with Time Exceeded *******************************/
+var minWindow = function(s, t) {
+    if(t.length > s.length) return "";
+
+    let charsNeeded = {};
+    for(let idx in t){
+        charsNeeded[t[idx]] = (charsNeeded[t[idx]] || 0) + 1
+    }
+
+    let leftP = 0,
+        rightP = 0,
+        min = [];
+    
+    let numOfNeeded = t.length;
+
+    while(rightP < s.length){
+        if(charsNeeded[s[rightP]] > 0){
+            numOfNeeded--;
+            charsNeeded[s[rightP]]--;
+        }
+        if(rightP-leftP >= t.length-1){
+            if(numOfNeeded === 0){
+                let dist = rightP-leftP;
+                let currentMin = (min[0] || Infinity)-(min[1] || Infinity);
+                let res = dist >= currentMin;
+                if(res) min = [rightP,leftP];
+                if(s[leftP] in charsNeeded){
+                    numOfNeeded++;
+                    charsNeeded[s[leftP]]++;
+                }
+                leftP++;
+            }
+        }
+        while(leftP < rightP || !(s[leftP] in charsNeeded)){
+            if(s[leftP] in charsNeeded) continue;
+            else leftP++;
+        }
+        rightP++;
+    }
+    return min.length === 0 ? "" : s.slice(min[0], min[1] + 1);
+};
+
+/**
+ * Notes with why this first code was failing
+ * 1. In the original code, the condition rightP - leftP >= t.length - 1 checks if 
+ * the window size is equal to or larger than the length of string t. However, 
+ * this condition is not sufficient. It should be modified to 
+ * rightP - leftP + 1 >= t.length to ensure that the window size is exactly equal to the length of t.
+ * 2. The nested while loop (leftP < rightP) is causing inefficient iterations. 
+ * The purpose of this loop is to move the left pointer leftP until it reaches the 
+ * right pointer or encounters a character that is in charsNeeded. However, 
+ * the current implementation can lead to excessive iterations and result in a time limit exceeded error.
+ */
+
+/**************** 2nd Iteration ********/
+/******************** Accepted *******************************/
+var minWindow = function(s, t) {
+    if (t.length > s.length) return "";
+
+    let charsNeeded = {};
+    for (let idx in t) {
+        charsNeeded[t[idx]] = (charsNeeded[t[idx]] || 0) + 1;
+    }
+
+    let leftP = 0,
+        rightP = 0,
+        min = [];
+
+    let numOfNeeded = t.length;
+    let minLength = Infinity;
+
+    while (rightP < s.length) {
+        if (s[rightP] in charsNeeded) {
+            if (charsNeeded[s[rightP]] > 0) {
+                numOfNeeded--;
+            }
+            charsNeeded[s[rightP]]--;
+        }
+
+        while (numOfNeeded === 0) {
+            if (rightP - leftP + 1 < minLength) {
+                minLength = rightP - leftP + 1;
+                min = [leftP, rightP];
+            }
+
+            if (s[leftP] in charsNeeded) {
+                charsNeeded[s[leftP]]++;
+                if (charsNeeded[s[leftP]] > 0) {
+                    numOfNeeded++;
+                }
+            }
+            leftP++;
+        }
+
+        rightP++;
+    }
+
+    return min.length === 0 ? "" : s.slice(min[0], min[1] + 1);
+};
+
+/** Updates with 2nd Iteration **/
+/**
+ * Same as before up to numOfNeeded declaration
+ * Added minLen as Inifinity: purpose so you dont need to check if it is the first time
+ *      and be afraid of undefined that may appear if min[0] does not exist
+ * 
+ * While rightP is less than the length of s
+ *      if rightP char is in charsNeeded
+ *          if charsNeeded's rightP char is > 0
+ *              decrement numOfNeeded
+ *          charsNeeded's rightP char value decrements
+ *      while neededLetters is satisfied
+ *          if rightP - leftP + 1 is less than minLen
+ *              change minLen and the array of min len
+ *          if s[leftP] is in charsNeeded 
+ *              *i.e leftP needs to be moved as we are not at the end and needs to be moved to valid location
+ *              increment charsNeeded leftP char value bc leftP char is not part of window anymore
+ *              if charsNeeded[s[leftP]] is greater than 0
+ *                  numOfNeeded++
+ *              leftP++ (move on to next unless still valid window, then we can shrink window more)
+ *      rightP++
+ * return if min array has nothing, then no substr so return "" or slice the string from min[0] to min[1] inclusively
+ ***Purpose of if charsNeeded[s[leftP]] is greater than 0
+ *  - Algo triers to minimize the window size by moving leftP towards the right
+ *      with each iteration of this loop, check if the current char at s[leftP] is in charsNeeded.
+ *      if it is, means that removing the character from window will make the window invalid.
+ *      However, simply checking if s[leftP] is in charsNeeded is not sufficient to determine if the char is 
+ *      needed or not. Possible that the count of char in charsNeeded is already zero before current iteration
+ *      idicating that the character is not needed anymore. Therefor check if character is actually needed.
+ *      With this check, ensure that we only increment numOfNeeded when the character at s[leftP] is needed
+ *          ie. its count in charsNeeded is greater than zero
+ *      The second while loop will then end. And we can move from the current window and see if there is a better min result
+ *      
+ */
