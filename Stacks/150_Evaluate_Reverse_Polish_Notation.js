bea29052dbb6022405423f66cfc968b03bb48981
Ezra Chung <ezra.chung97@gmail.com>
2023-07-07 01:34:18 -0700

    Resolve while loop

--- Stacks/150_Evaluate_Reverse_Polish_Notation.js
+++ Stacks/150_Evaluate_Reverse_Polish_Notation.js
@@ -3,7 +3,38 @@
  * @return {number}
  */
 var evalRPN = function(tokens) {
-    
+    if(tokens.length < 2) {
+        return (Number(tokens[0]) || 0)}
+    let stack = [];
+    let operators = ['+', '-', '*','/'];
+    stack.push(tokens[0]);
+    stack.push(tokens[1]);
+    let i = 2;
+    while(stack.length >= 1){ //Error with this
+        // console.log(tokens[i], stack)
+        if(operators.indexOf(tokens[i]) >= 0){
+            let second = Number(stack.pop());
+            let first = Number(stack.pop());
+            console.log(second, stack, "here")
+            // console.log(first, second, tokens[i])
+            let result;
+            if(tokens[i] === '+') result = first+second
+            else if (tokens[i] === '-') result = first-second
+            else if (tokens[i] === '*') result = first*second
+            else if (tokens[i] === '/') {
+                //should truncate toward zero
+                if(second > 0) result = Math.floor(first/second)
+                else result = Math.ceil(first/second) //Results in negative 0, if negative second
+            }
+            if(i === tokens.length-1) return result
+            stack.push(result);
+        }
+        else{
+            stack.push(Number(tokens[i]))
+        }
+        i++;
+        // console.log(stack,stack.length, "at the end")
+    }
 };
 
 /**
