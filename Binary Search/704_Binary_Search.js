dfa3e6fda91be4398639d1f3abe4c1a655ef2593
Ezra Chung <ezra.chung97@gmail.com>
2023-07-07 18:38:09 -0700

    Notes

--- Binary Search/704_Binary_Search.js
+++ Binary Search/704_Binary_Search.js
@@ -6,3 +6,27 @@
 var search = function(nums, target) {
     
 };
+
+/**
+Problem
+with array, find index of target. Else return -1
+
+Notes
+
+Code
+left = 0
+right = nums.length
+while(left <= right)
+    mid is Math.floor(left+right/2)
+    chosen is nums[mid]
+
+    check if chosen is equal to target
+    if true, return mid
+
+    if chosen is less than target
+    reassign left to equal mid+1
+
+    chosen is greater than ra
+    reasign right to equal mid-1
+return -1
+*/
