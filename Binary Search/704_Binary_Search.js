09610fee0c9e37bacb91707d5c65ff82594c01a6
Ezra Chung <ezra.chung97@gmail.com>
2023-07-07 22:12:20 -0700

    Accepted result

--- Binary Search/704_Binary_Search.js
+++ Binary Search/704_Binary_Search.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    
+    let left = 0;
+    let right = nums.length-1;
+    while(left <= right){
+        let mid = Math.floor((left+right)/2);
+        if(nums[mid] === target) return mid;
+        if(nums[mid] < target) left = mid+1;
+        if(nums[mid] > target) right = mid-1;
+    }
+    return -1;
 };
 
 /**
