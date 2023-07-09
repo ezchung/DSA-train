3e54fb83129519330cb0a5d919ae31bbd4a36b86
Ezra Chung <ezra.chung97@gmail.com>
2023-07-09 01:34:41 -0700

    Accepted

--- Binary Search/153_Find_Min_in_Rotated_Sorted_Arr.js
+++ Binary Search/153_Find_Min_in_Rotated_Sorted_Arr.js
@@ -3,8 +3,29 @@
  * @return {number}
  */
 var findMin = function(nums) {
-    
+    return recurseNums(0, nums.length-1, nums)
 };
+
+let recurseNums = (leftP, rightP, nums) => {
+    let mid = Math.floor((leftP+rightP)/2)
+    //if leftP is greater than rightP, means the search space has been exhausted and return nums[0]
+    if(leftP > rightP) {
+        return nums[0];
+    }
+
+    //If nums[mid] > nums[mid+1], found min point
+    if(nums[mid] > nums[mid+1]) {
+        return nums[mid+1];}
+    
+    if(nums[mid] < nums[mid-1]) {
+        return nums[mid];}
+
+    //If nums[mid] > nums[0], recurse with left moving up
+    //else nums[mid] < nums[length-1], right moves down
+    if(nums[mid] > nums[0]) return recurseNums(mid+1, rightP, nums);
+    return recurseNums(leftP, mid-1, nums);
+}
+
 
 /**
 Problem
@@ -42,3 +63,48 @@ baseCase
     else
         recursive()
 */
+
+/**** Failed due to Runtime Error. Loop is not defined properly */
+var findMinRE = function(nums) {
+    return recurseNumsRE(0, nums.length-1, nums)
+};
+
+let recurseNumsRE = (leftP, rightP, nums) => {
+    //if leftP is greater than rightP, means the search space has been exhausted and return nums[0]
+
+    //check if already sorted
+    //if nums[leftP] less than or equal to nums[rightP], subarray sorted and return nums[leftP] as min
+
+    let res = false;
+    while(!res){
+        let mid = Math.floor((leftP+rightP)/2);
+        let leftNum = nums[mid-1] || NaN;
+        let rightNum = nums[mid+1] || NaN;
+        //Check if at edge and no results means its at the other half. go to the other side and check
+        if(!leftNum){
+            leftNum = nums[nums.length-1];
+            if(nums[mid] > leftNum && nums[mid] > rightNum){
+                res = true;
+                return rightNum;
+            }
+            return;
+        }else if(!rightNum){
+            rightNum = nums[0];
+            if(nums[mid] > leftNum && nums[mid] > rightNum){
+                res = true;
+                return rightNum;
+            }
+            return;
+        }
+        //When not at edges
+        if(nums[mid] > leftNum && nums[mid] > rightNum){
+            res = true;
+            return rightNum;
+        }else{
+            //Currently not ending
+            recurseNumsRE(mid+1, rightP, nums)
+            recurseNumsRE(leftP, mid-1, nums)
+        }
+        res = true
+    }
+}
