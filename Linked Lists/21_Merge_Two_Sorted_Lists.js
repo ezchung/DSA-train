8c4a5bf1f6b06293e4fe925ea33a1763ae227c17
Ezra Chung <ezra.chung97@gmail.com>
2023-07-10 01:02:34 -0700

    Notes

--- Linked Lists/21_Merge_Two_Sorted_Lists.js
+++ Linked Lists/21_Merge_Two_Sorted_Lists.js
@@ -13,3 +13,26 @@
 var mergeTwoLists = function(list1, list2) {
     
 };
+
+/**
+Look at the two lists. 
+There are two pointers
+create new linked list
+let list1Curr = list1
+let list2Curr = list2
+let l1prev = null;
+let l2prev = null;
+let prev = null
+let newLLCurr = null
+while(list1Curr || list2Curr){
+    if(!list1Curr) pass rest of list2 into the new LL
+    if(list1Curr.val > list2Curr.val)
+        pass in list2Curr to newLL
+        prev.next = list2Curr //go to prev and assign next
+        prev = list2Curr
+        list2Curr = list2Curr.next
+    
+
+}
+
+ */
