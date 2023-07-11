5a4a01e2e29f5a2e7c14b9e790bbaa2a601db93b
Ezra Chung <ezra.chung97@gmail.com>
2023-07-10 18:10:54 -0700

    Notes

--- Trees/226_Invert_Binary_tree.js
+++ Trees/226_Invert_Binary_tree.js
@@ -13,3 +13,33 @@
 var invertTree = function(root) {
     
 };
+
+/**
+Problem
+- Reverse the binary tree
+
+Algorithm
+
+
+Idea:
+Maybe working recursively
+switch left and right 
+once there is no children return
+if there is only one,
+    if it is on the left, move to right. if right move to left
+    
+
+Code
+invertTree(root){
+    if(!root) return;
+
+    let temp = root.left
+    root.left = root.right;
+    root.right = temp;
+    if(root.left) invertTree(root.left)
+    if(root.right) invertTree(root.right)
+    return root
+
+}
+
+ */
