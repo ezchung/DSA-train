c4b39af73156b2003657793e1444eed88acd3efc
Ezra Chung <ezra.chung97@gmail.com>
2023-07-10 20:06:45 -0700

    Accepted recursive

--- Trees/226_Invert_Binary_tree.js
+++ Trees/226_Invert_Binary_tree.js
@@ -11,7 +11,14 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    
+    if(!root) return null;
+
+    let temp = root.left
+    root.left = root.right;
+    root.right = temp;
+    if(root.left) invertTree(root.left)
+    if(root.right) invertTree(root.right)
+    return root
 };
 
 /**
