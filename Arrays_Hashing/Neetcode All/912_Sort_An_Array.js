4edab931a494f33aa080652c986043576ad75cf0
Ezra Chung <ezra.chung97@gmail.com>
2023-07-28 12:26:06 -0700

    Working solution. Needed to change the mid

--- Arrays_Hashing/Neetcode All/912_Sort_An_Array.js
+++ Arrays_Hashing/Neetcode All/912_Sort_An_Array.js
@@ -3,7 +3,27 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    
+    function mergeSort(arr){
+        if(arr.length <= 1) return arr;
+        let mid = Math.floor((arr.length)/2);
+        let left = arr.slice(0, mid);
+        let right = arr.slice(mid);
+        return merge(mergeSort(left), mergeSort(right));
+    }
+
+    function merge(left, right){
+        let sorted = [];
+        while(left.length && right.length){
+            if(left[0] < right[0]){
+                sorted.push(left.shift());
+            }else{
+                sorted.push(right.shift());
+            }
+        }
+        return [...sorted, ...left, ...right];
+    }
+
+    return mergeSort(nums);
 };
 
 /*
