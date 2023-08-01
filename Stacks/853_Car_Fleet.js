e9a7f6c23fbf0c724cacdc270c292e4c0bbd1766
Ezra Chung <ezra.chung97@gmail.com>
2023-08-01 11:23:05 -0700

    Notes

--- Stacks/853_Car_Fleet.js
+++ Stacks/853_Car_Fleet.js
@@ -16,17 +16,21 @@ A car can never pass another car.
 - so only if the car ahead catches up to a car before it, technically car behind the first cannot pass
 the higher the position, the more ahead it is.
 Target number is the position
+once they meet, faster car will slow down
 
 
 Psuedo
 Stack
 if position is only one, return 1
 else
-create stack
+create stack (LIFO)
+
 
 Others
 - create an array to signify positions
 - idea is that every iteration, each position is given its miles gained.
     - once we see the target number reached, any number at that target number is removed and continues til target number is exceeded or reached 
 
+- once one meets the previous, must stay together and faster car's speed is brought down to slower car speed. 
+    - if another car catches up, then the two that are already there are given slower speed.
 */
