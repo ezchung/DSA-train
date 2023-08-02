6ff441503aba3447e78d020e4d1e12074d1ccd5c
Ezra Chung <ezra.chung97@gmail.com>
2023-08-02 15:28:26 -0700

    Add notes

--- Stacks/853_Car_Fleet.js
+++ Stacks/853_Car_Fleet.js
@@ -19,7 +19,7 @@ Target number is the position
 once they meet, faster car will slow down
 
 
-Psuedo
+Code Notes
 Stack
 if position is only one, return 1
 else
@@ -33,4 +33,32 @@ Others
 
 - once one meets the previous, must stay together and faster car's speed is brought down to slower car speed. 
     - if another car catches up, then the two that are already there are given slower speed.
+
+create a tuple array of position and speed
+- insert into stack
+- position keeps getting added based on speed
+- if the one behind catches up, then speed is changed to be slower speed compared to the two and categorized as one, meaning we can remove one of them.
+
+Intuition 
+- calculate the finish time of each car(if they do not get blocked) 
+    - if car A were before B initially, but A's finish time is less than or equal to B's, then A & B must be same fleet
+>>Sort cars by initial position in dcrs order
+>>First car leads a fleet. first car is current leader
+>>iterate to next cars and see if they belong with current leader fleet
+>>if car X violates the condition, X is new leader and repeat.
+----Number of fleets is the number of updates to current leader
+
+Pseudo
+if position is one, return 1
+else
+organize the position and speed
+create array of tuples where position and speed are aligned
+create stack = []
+iterate through the array of pairs
+    get position, speed, time
+    **time is the (target-position)/speed
+    push time into stack
+    if stack length is greater than 2 and end of stack is less than one before
+        **end of stack <= [stack.len-2] bc time is less than one that had a greater starting point. Caught up
+        pop stack
 */
