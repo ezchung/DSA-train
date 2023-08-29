8ffde101dc59a154efdd343117cb88863d1fe8d2
Ezra Chung <ezra.chung97@gmail.com>
2023-08-29 13:14:36 -0700

    Practice

---
+++ Sliding Window/Sliding_window_prac.js
@@ -0,0 +1,19 @@
+/*
+Write a function which accepts an array of integers and a number called n. The function 
+should calculate the maximum sum of n consecutive elements in the array
+
+Get the max sum of any n consecutive numbers in the array.
+
+Ex
+maxSubarraySum([1,2,5,2,8,1,5],2) //10
+
+*/
+
+function maxSubarraySum([arr, n]){
+
+}
+
+/*
+O(n^2)
+would use a nested for loop
+*/
