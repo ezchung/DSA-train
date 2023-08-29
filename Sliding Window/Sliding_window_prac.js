4a7bb3375dda6f5bac16bc865b0f16c5a3f3190d
Ezra Chung <ezra.chung97@gmail.com>
2023-08-29 13:18:57 -0700

    naive solution

--- Sliding Window/Sliding_window_prac.js
+++ Sliding Window/Sliding_window_prac.js
@@ -16,4 +16,27 @@ function maxSubarraySum([arr, n]){
 /*
 O(n^2)
 would use a nested for loop
+
+create variable max as -Infinity
+iterate through arr as long as i < arr length - n
+    with example above of 10
+        i < 7 - 2 + 1 //purpose because by this idx we will know our answer and 
+        a new answer cannot be made after this point
 */
+
+function naiveMaxSubarraySum([arr,n]){
+    if(n > arr.length) return null;
+
+    let max = -Infinity;
+    for(let i = 0; i < arr.length-n+1;i++){
+        temp = 0;
+        for(let j = 0; j < num; j++){
+            temp += arr[i+j];
+        }
+        if(temp > max){
+            max = temp;
+        }
+    }
+    
+    return max;
+}
