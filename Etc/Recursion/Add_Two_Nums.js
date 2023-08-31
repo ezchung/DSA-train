ffe3303795975705427a320ef9c2d1e6524162eb
Ezra Chung <ezra.chung97@gmail.com>
2023-08-30 21:00:17 -0700

    problem add two numbers

---
+++ Etc/Recursion/Add_Two_Nums.js
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let num1 = +createArr(l1);
+    let num2 = +createArr(l2);
+    let result = num1 + num2;
+    let final = createLL(result);
+    return final
+};
+
+let createArr = function(linkedList){
+    let value = linkedList;
+    let organizedArr = []
+    while(value !== null){
+        organizedArr.unshift(value.val);
+        value = value.next;
+    }
+    return organizedArr.join("");
+}
+
+let createLL = function(num){
+    let text = Array.from(String(num)).reverse();
+    const head = new ListNode(text[0]);
+    let curr = head;
+    console.log(text, "Text")
+    for(let n = 1; n < text.length; n++){
+        const newNode = new ListNode(text[n]);
+        curr.next = newNode;
+        curr = newNode;
+    }
+    return head;
+}
+
+/* 
+iterate through the linked lists and create a string of the numbers
+*/
